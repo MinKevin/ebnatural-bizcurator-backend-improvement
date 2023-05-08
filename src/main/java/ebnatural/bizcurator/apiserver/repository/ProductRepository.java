29ba9212db09905398f2b5acb097fbc68bb9635f
itstimi-XD <ssk0710402@gmail.com>
2023-05-08 21:57:12 +0900

    Feat: Product Entity, Repository class 추가
    related to: #3

---
+++ src/main/java/ebnatural/bizcurator/apiserver/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package ebnatural.bizcurator.apiserver.repository;
+
+import ebnatural.bizcurator.apiserver.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
