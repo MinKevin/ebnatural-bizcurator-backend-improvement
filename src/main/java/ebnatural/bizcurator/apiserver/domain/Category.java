c28e37ff9f4337a001d25620dbcb769a98e00546
itstimi-XD <ssk0710402@gmail.com>
2023-05-08 21:55:19 +0900

    Feat: #3 Category Entity, Repository class 추가
    related to: #3

---
+++ src/main/java/ebnatural/bizcurator/apiserver/domain/Category.java
@@ -0,0 +1,24 @@
+package ebnatural.bizcurator.apiserver.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "category")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+}
---
+++ src/main/java/ebnatural/bizcurator/apiserver/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package ebnatural.bizcurator.apiserver.repository;
+
+
+import ebnatural.bizcurator.apiserver.domain.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
