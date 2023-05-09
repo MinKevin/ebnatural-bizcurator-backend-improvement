7d2a41e359bba0f61c27e530b3edef03c5b37585
chan <seulchan07@gmail.com>
2023-05-09 13:56:50 +0900

    Refactor: 사용하지 않는 import문 정리
    related to: #4

--- src/main/java/ebnatural/bizcurator/apiserver/domain/Article.java
+++ src/main/java/ebnatural/bizcurator/apiserver/domain/Article.java
@@ -10,8 +10,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Index;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
