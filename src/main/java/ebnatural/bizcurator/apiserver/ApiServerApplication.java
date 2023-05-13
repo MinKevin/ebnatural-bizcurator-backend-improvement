297df4ada4fb7a3549f479dee41418f791ec2c6e
YeonHee <lyh951212@naver.com>
2023-05-13 15:56:17 +0900

    Chore: Deploy test

--- src/main/java/ebnatural/bizcurator/apiserver/ApiServerApplication.java
+++ src/main/java/ebnatural/bizcurator/apiserver/ApiServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class ApiServerApplication {
+public class ApiServerApplication { 
     public static void main(String[] args) {
         SpringApplication.run(ApiServerApplication.class, args);
     }
