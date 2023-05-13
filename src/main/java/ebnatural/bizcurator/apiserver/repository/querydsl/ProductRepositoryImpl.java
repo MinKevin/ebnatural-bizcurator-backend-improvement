7d85d3e2ea879be051b549371176d945bc242271
itstimi-XD <ssk0710402@gmail.com>
2023-05-13 21:07:16 +0900

    Refactor: 불필요한 Import문 정리 및  변수명 변경
    Entity Path 추가는 불필요한 것이었어서 빼고, 아까와 마찬가지로 snake case -> camel case로 변경
    related to: #26

--- src/main/java/ebnatural/bizcurator/apiserver/repository/querydsl/ProductRepositoryImpl.java
+++ src/main/java/ebnatural/bizcurator/apiserver/repository/querydsl/ProductRepositoryImpl.java
@@ -1,9 +1,11 @@
 package ebnatural.bizcurator.apiserver.repository.querydsl;
 
-import com.querydsl.core.types.EntityPath;
 import com.querydsl.jpa.impl.JPAQuery;
 import ebnatural.bizcurator.apiserver.domain.Product;
+import ebnatural.bizcurator.apiserver.domain.QProduct;
+import ebnatural.bizcurator.apiserver.domain.QProductImage;
 import ebnatural.bizcurator.apiserver.dto.response.ProductResponse;
+import ebnatural.bizcurator.apiserver.dto.response.QProductResponse;
 import java.util.List;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
@@ -17,33 +19,31 @@ public class ProductRepositoryImpl extends QuerydslRepositorySupport implements 
         QProduct product = QProduct.product;
         QProductImage productImage = QProductImage.productImage;
 
-        JPAQuery<ProductResponse> query = from(product)
-                .leftJoin((EntityPath) productImage).on(product.id.eq(productImage.product.id).and(productImage.repimgYn.eq("Y")))
+        JPAQuery<ProductResponse> query = (JPAQuery<ProductResponse>) from(product)
+                .leftJoin(productImage).on(product.id.eq(productImage.product.id).and(productImage.repimgYn.eq("Y")))
                 .select(new QProductResponse(
                         product.id,
                         product.category.id,
                         product.name,
                         productImage.id,
-                        productImage.image_url,
-                        product.regular_price,
-                        product.discount_rate,
-                        product.regular_price.subtract(
-                                product.regular_price.multiply(product.discount_rate).divide(100)
+                        productImage.imgUrl,
+                        product.regularPrice,
+                        product.discountRate
                         )
-                ));
+                );
 
         if (categoryId != null) {
             query.where(product.category.id.eq(categoryId));
         }
 
         if ("new".equals(sort)) {
-            query.orderBy(product.created_at.desc());
+            query.orderBy(product.createdAt.desc());
         } else if ("popular".equals(sort)) {
-            query.orderBy(product.weekly_clicks.desc());
+            query.orderBy(product.weeklyClicks.desc());
         } else if ("low_price".equals(sort)) {
-            query.orderBy(product.regular_price.asc());
+            query.orderBy(product.regularPrice.asc());
         } else if ("high_price".equals(sort)) {
-            query.orderBy(product.regular_price.desc());
+            query.orderBy(product.regularPrice.desc());
         }
 
         return query.fetch();
