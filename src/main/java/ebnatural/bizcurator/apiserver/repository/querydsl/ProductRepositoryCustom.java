6e1f38aabf29253ccd03a8e8a1757f805dbb3c86
itstimi-XD <ssk0710402@gmail.com>
2023-05-13 16:38:28 +0900

    Feat: ProductRepositoryCustom 추가
    상품목록 조회 API개발을 위한 추가.
    related to: #26

---
+++ src/main/java/ebnatural/bizcurator/apiserver/repository/querydsl/ProductRepositoryCustom.java
@@ -0,0 +1,9 @@
+package ebnatural.bizcurator.apiserver.repository.querydsl;
+
+import ebnatural.bizcurator.apiserver.dto.response.ProductResponse;
+import java.util.List;
+
+public interface ProductRepositoryCustom {
+    List<ProductResponse> findByCategoryId(Long categoryId, String sort);
+
+}
