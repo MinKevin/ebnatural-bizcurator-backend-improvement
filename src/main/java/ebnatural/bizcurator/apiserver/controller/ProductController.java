5c1921f958473d375fe343c8433fc6206e9a2d8d
itstimi-XD <ssk0710402@gmail.com>
2023-05-13 23:35:42 +0900

    Refactor: 생성자 삭제 및 @RequiredConstructor 추가
    related to: #26

--- src/main/java/ebnatural/bizcurator/apiserver/controller/ProductController.java
+++ src/main/java/ebnatural/bizcurator/apiserver/controller/ProductController.java
@@ -3,6 +3,7 @@ package ebnatural.bizcurator.apiserver.controller;
 import ebnatural.bizcurator.apiserver.dto.response.ProductResponse;
 import ebnatural.bizcurator.apiserver.repository.ProductRepository;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +11,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/products")
 public class ProductController {
     private final ProductRepository productRepository;
-
-    public ProductController(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
-
+    
     @GetMapping
     public ResponseEntity<List<ProductResponse>> getProducts(
             @RequestParam(required = false) Long categoryId,
