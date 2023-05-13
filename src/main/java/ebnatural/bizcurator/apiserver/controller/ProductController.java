434614c33646367aad0289bbe88fb7e3cd3985ec
itstimi-XD <ssk0710402@gmail.com>
2023-05-13 23:53:15 +0900

    Refactor: Move DB operations from ProductController to ProductService
    이전에는 ProductController에서 수행하던 Database operation을 이번에 생성된 ProductService로 이사시킴. 이제 ProductController는 product들의 데이터를 얻기위해 ProductService를 호출함. 해당 수정내용은 Service단에서 DB 작업을 하는 관행이 반영됨.
    
    + DB 작업을 처리하는 getProducts 메서드가 있는 ProductService 클래스 생성
    + ProductController가 ProductService를 사용하여 제품 데이터를 가져오도록 재구성
    
    related to: #26

--- src/main/java/ebnatural/bizcurator/apiserver/controller/ProductController.java
+++ src/main/java/ebnatural/bizcurator/apiserver/controller/ProductController.java
@@ -2,6 +2,7 @@ package ebnatural.bizcurator.apiserver.controller;
 
 import ebnatural.bizcurator.apiserver.dto.response.ProductResponse;
 import ebnatural.bizcurator.apiserver.repository.ProductRepository;
+import ebnatural.bizcurator.apiserver.service.ProductService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,13 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/products")
 public class ProductController {
-    private final ProductRepository productRepository;
-    
+    private final ProductService productService;
+
     @GetMapping
     public ResponseEntity<List<ProductResponse>> getProducts(
             @RequestParam(required = false) Long categoryId,
             @RequestParam(defaultValue = "new") String sort) {
-        List<ProductResponse> products = productRepository.findByCategoryId(categoryId, sort);
+        List<ProductResponse> products = productService.getProducts(categoryId, sort);
         return ResponseEntity.ok(products);
     }
 }
---
+++ src/main/java/ebnatural/bizcurator/apiserver/service/ProductService.java
@@ -0,0 +1,17 @@
+package ebnatural.bizcurator.apiserver.service;
+
+import ebnatural.bizcurator.apiserver.dto.response.ProductResponse;
+import ebnatural.bizcurator.apiserver.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<ProductResponse> getProducts(Long categoryId, String sort) {
+        return productRepository.findByCategoryId(categoryId, sort);
+    }
+}
