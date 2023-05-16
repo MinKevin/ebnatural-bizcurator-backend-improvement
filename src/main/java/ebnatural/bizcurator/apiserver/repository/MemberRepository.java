957c8554157e36580c662a80eee76813c5645117
MinKevin <alswkdrb1@naver.com>
2023-05-16 22:07:05 +0900

    enhancement: 코드리뷰 적용 v2

--- src/main/java/ebnatural/bizcurator/apiserver/repository/MemberRepository.java
+++ src/main/java/ebnatural/bizcurator/apiserver/repository/MemberRepository.java
@@ -6,19 +6,19 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
-
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByUsername(String userName);
     Optional<Member> findById(Long id);
-    @Transactional
-    @Modifying
-    @Query(value = "update member set last_login = now() where username = :username", nativeQuery = true)
-    public int updateMemberLastLogin(@Param("username") String username);
 
     @Transactional
     @Modifying(clearAutomatically = true)
     @Query("update Member u set u.refreshToken = ?1 where u.id = ?2")
     void updateRefreshToken(String newRefreshToken, Long id);
+
+    //todo 수정
+//    @Query("SELECT COUNT(m) FROM Member m WHERE m.lastLoginTime = :currentDate")
+    //int countByLastLoginTime(@Param("currentDate") LocalDate currentDate);
 }
