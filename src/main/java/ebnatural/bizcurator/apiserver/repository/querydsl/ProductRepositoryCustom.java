cdbdc38d97184086dc05b2eab99040a8a049b7f2
itstimi-XD <ssk0710402@gmail.com>
2023-05-16 17:34:49 +0900

    Feat: findDetailById 구현
    service에서 하기보다는 기존에도 repoimpl에서 구현했기 때문에 이와같이 구현함.
    related to: #34

--- src/main/java/ebnatural/bizcurator/apiserver/repository/querydsl/ProductRepositoryCustom.java
+++ src/main/java/ebnatural/bizcurator/apiserver/repository/querydsl/ProductRepositoryCustom.java
@@ -1,8 +1,10 @@
 package ebnatural.bizcurator.apiserver.repository.querydsl;
+import ebnatural.bizcurator.apiserver.dto.ProductDetailDto;
 import ebnatural.bizcurator.apiserver.dto.ProductListDto;
 import java.util.List;
 
 public interface ProductRepositoryCustom {
     List<ProductListDto> findByCategoryId(Long categoryId, String sort);
     List<ProductListDto> searchByKeyword(String keyword, String sort);
+    ProductDetailDto findDetailById(Long productId);
 }
