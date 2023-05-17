85c3adef7747b995a6867cffd9c668834b943578
YeonHee <lyh951212@naver.com>
2023-05-17 19:33:42 +0900

    Feat: 관리자페이지 주문배송 검색

--- src/main/java/ebnatural/bizcurator/apiserver/repository/ProductRepository.java
+++ src/main/java/ebnatural/bizcurator/apiserver/repository/ProductRepository.java
@@ -4,8 +4,12 @@ import ebnatural.bizcurator.apiserver.domain.Product;
 import ebnatural.bizcurator.apiserver.repository.querydsl.ProductRepositoryCustom;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
 
     public Optional<Product> findById(Long id);
+    @Query("SELECT p.manufacturer, p.category FROM Product p WHERE p.id = :id")
+    Optional<Object[]> findManufacturerAndCategoryById(@Param("id") Long id);
 }
--- src/main/java/ebnatural/bizcurator/apiserver/service/AdminOrderService.java
+++ src/main/java/ebnatural/bizcurator/apiserver/service/AdminOrderService.java
@@ -1,17 +1,34 @@
 package ebnatural.bizcurator.apiserver.service;
 
+import ebnatural.bizcurator.apiserver.domain.Category;
+import ebnatural.bizcurator.apiserver.domain.Manufacturer;
+import ebnatural.bizcurator.apiserver.domain.OrderDetail;
+import ebnatural.bizcurator.apiserver.domain.Product;
 import ebnatural.bizcurator.apiserver.dto.AdminHomeInfoDto;
+import ebnatural.bizcurator.apiserver.dto.AdminOrderDetailDto;
 import ebnatural.bizcurator.apiserver.repository.MemberRepository;
+import ebnatural.bizcurator.apiserver.repository.OrderDetailRepository;
+import ebnatural.bizcurator.apiserver.repository.ProductRepository;
+import java.awt.print.Pageable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class AdminOrderService {
 
+    private final int PAGE_SIZE = 6;
     private final MemberRepository memberRepository;
+    private final OrderDetailRepository orderDetailRepository;
+
+    private final ProductRepository productRepository;
     /**
      * 관리자페이지 홈 화면 정보를 반환
      * @return
@@ -21,5 +38,47 @@ public class AdminOrderService {
                 (int) memberRepository.count(),
                 memberRepository.countByLastLoginTime(LocalDate.now())
         );
+    }
+
+    /**
+     * 주문내역 리스트를 페이지네이션과 검색 키워드를 통해 반환
+     * @return
+     */
+    public List<AdminOrderDetailDto> showOrderDetailListByPageIndexAndSearchKeyword(Integer page, String search) {
+        if (null == page) {
+            page = 1;
+        }
+
+        PageRequest pageable = PageRequest.of(page, PAGE_SIZE);
+        Page<OrderDetail> orderDetailPage = null;
+        if (search == null) {
+            orderDetailPage = orderDetailRepository.findAllByOrderByOrderTimeDesc(pageable);
+        } else{
+            orderDetailPage = orderDetailRepository.findAllByProduct_NameContainingIgnoreCaseOrderByOrderTimeDesc(search, pageable);
+        }
+
+        List<AdminOrderDetailDto> adminOrderDetailDtoList = new ArrayList<>();
+        for (OrderDetail orderDetail : orderDetailPage) {
+            Product product = orderDetailRepository.findProductById(orderDetail.getId());
+            Optional<Object[]> result = productRepository.findManufacturerAndCategoryById(product.getId());
+            Manufacturer manufacturer = (Manufacturer) result.get()[0];
+            Category category = (Category) result.get()[1];
+
+            AdminOrderDetailDto adminOrderDetailDto = AdminOrderDetailDto.of(
+                    orderDetail.getId(),
+                    product.getName(),
+                    manufacturer.getName(),
+                    category.getName(),
+                    orderDetail.getOrderTime().toString(),
+                    orderDetail.getDeliveryState().getMeaning(),
+                    orderDetail.getQuantity(),
+                    orderDetail.getCost(),
+                    orderDetail.getInvoiceNumber()
+            );
+
+            adminOrderDetailDtoList.add(adminOrderDetailDto);
+        }
+
+        return adminOrderDetailDtoList;
     }
 }
