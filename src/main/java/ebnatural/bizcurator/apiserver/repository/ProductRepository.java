c9d058b8cadf351753a27e683f6248a0f1bf3b8d
itstimi-XD <ssk0710402@gmail.com>
2023-05-17 13:58:02 +0900

    Refactor: findById 삭제
    - JpaRepository 인터페이스는 여러 CRUD 작업을 위한 메소드를 이미 제공함, findById(ID id) 메소드도 포함되어 있음. 이 메소드는 주어진 ID에 해당하는 엔티티를 검색하고, 해당 엔티티가 있으면 Optional에 담아서 반환함
    -  findById(Long id)가 JpaRepository에서 제공하는 기본 메소드와 동일한 시그니처를 가지므로, 이를 별도로 선언할 필요가 없을 것으로 보여 연희님과 상의 후 삭제.
    
    related to: #66

--- src/main/java/ebnatural/bizcurator/apiserver/repository/ProductRepository.java
+++ src/main/java/ebnatural/bizcurator/apiserver/repository/ProductRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
 
-    public Optional<Product> findById(Long id);
+    @Modifying
 }
