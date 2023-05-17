1b2855595a5bb87ef99f210397a57b3fb6957926
chan <seulchan07@gmail.com>
2023-05-17 19:28:33 +0900

    Fix: 프론트팀 요청으로 토큰 인증 기능 중단
    - 프론트팀 로그인 기능 미구현으로 인증 기능 잠시 중단

--- src/main/java/ebnatural/bizcurator/apiserver/common/config/SpringSecurityConfig.java
+++ src/main/java/ebnatural/bizcurator/apiserver/common/config/SpringSecurityConfig.java
@@ -1,7 +1,5 @@
 package ebnatural.bizcurator.apiserver.common.config;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import ebnatural.bizcurator.apiserver.common.exception.FilterExceptionHandler;
 import ebnatural.bizcurator.apiserver.common.config.filter.JwtFilter;
 import ebnatural.bizcurator.apiserver.common.jwt.JwtProvider;
 import ebnatural.bizcurator.apiserver.repository.MemberRepository;
@@ -9,30 +7,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.AccessDeniedHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 
 @Configuration
@@ -48,34 +31,37 @@ public class SpringSecurityConfig {
         http.rememberMe().disable();
         http.cors();
         http.formLogin().disable();
-
         http.authorizeRequests()
-                .antMatchers("/login", "/signup", "/api/users/login", "/api/users/signup").permitAll()
-                .antMatchers("/api/admins/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.POST, "/api/notices/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/api/notices/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/api/notices/**").hasRole("ADMIN")
-                .anyRequest().authenticated();
+                .antMatchers("*").permitAll()
+                .anyRequest().permitAll();
 
-        http.exceptionHandling()
-                .authenticationEntryPoint(new AuthenticationEntryPoint() {
-                    @Override
-                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-                        // 인증문제가 발생했을 때 이 부분을 호출한다.
-                        throw authException;
-                    }
-                })
-                .accessDeniedHandler(new AccessDeniedHandler() {
-                    @Override
-                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-                        // 권한이 없는 페이지 요청을 하면 이 부분을 호출한다.
-                        throw accessDeniedException;
-                    }
-                });
-
-        http.addFilterBefore(jwtAuthorizationFilter(), BasicAuthenticationFilter.class);
-        http.addFilterBefore(new FilterExceptionHandler(), UsernamePasswordAuthenticationFilter.class);
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//        http.authorizeRequests()
+//                .antMatchers("/login", "/signup", "/api/users/login", "/api/users/signup").permitAll()
+//                .antMatchers("/api/admins/**").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.POST, "/api/notices/**").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.PUT, "/api/notices/**").hasRole("ADMIN")
+//                .antMatchers(HttpMethod.DELETE, "/api/notices/**").hasRole("ADMIN")
+//                .anyRequest().authenticated();
+//
+//        http.exceptionHandling()
+//                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+//                    @Override
+//                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+//                        // 인증문제가 발생했을 때 이 부분을 호출한다.
+//                        throw authException;
+//                    }
+//                })
+//                .accessDeniedHandler(new AccessDeniedHandler() {
+//                    @Override
+//                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+//                        // 권한이 없는 페이지 요청을 하면 이 부분을 호출한다.
+//                        throw accessDeniedException;
+//                    }
+//                });
+//
+//        http.addFilterBefore(jwtAuthorizationFilter(), BasicAuthenticationFilter.class);
+//        http.addFilterBefore(new FilterExceptionHandler(), UsernamePasswordAuthenticationFilter.class);
+//        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         return http.build();
     }
