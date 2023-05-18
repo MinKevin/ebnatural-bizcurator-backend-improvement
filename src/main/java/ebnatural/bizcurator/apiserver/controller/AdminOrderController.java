882350e99cf2488e0a669d7a33828f46a8ccfba3
YeonHee <lyh951212@naver.com>
2023-05-18 21:21:47 +0900

    Feat : 주문 취소 신청서 상태 처리

--- src/main/java/ebnatural/bizcurator/apiserver/controller/AdminOrderController.java
+++ src/main/java/ebnatural/bizcurator/apiserver/controller/AdminOrderController.java
@@ -3,6 +3,7 @@ package ebnatural.bizcurator.apiserver.controller;
 import ebnatural.bizcurator.apiserver.dto.AdminApplicationDto;
 import ebnatural.bizcurator.apiserver.dto.AdminHomeInfoDto;
 import ebnatural.bizcurator.apiserver.dto.AdminOrderDetailDto;
+import ebnatural.bizcurator.apiserver.dto.ApplicationChangeStateDto;
 import ebnatural.bizcurator.apiserver.dto.PaymentHistoryDto;
 import ebnatural.bizcurator.apiserver.dto.response.CommonResponse;
 import ebnatural.bizcurator.apiserver.service.AdminOrderService;
@@ -11,11 +12,17 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -86,5 +93,15 @@ public class AdminOrderController {
         historyMap.put("dataTotalCount", adminApplicationPair.getFirst());
         historyMap.put("histories", adminApplicationPair.getSecond());
         return CommonResponse.ok(HttpStatus.OK.value(), "관리자 페이지 주문 환불신청 조회 완료했습니다.", historyMap);
+    }
+
+    @Operation(summary = "주문 취소 신청서 상태 처리", description = "승인,거절 처리를 합니다.")
+    @PutMapping("/applications/cancellations/{application-id}")
+    public ResponseEntity<CommonResponse> changeStateCancelApplication(
+            @PathVariable("application-id") Long applicationId,
+            @Valid @RequestBody ApplicationChangeStateDto applicationChangeStateDto) {
+
+        adminOrderService.changeStateCancelApplication(applicationId, applicationChangeStateDto.getType().equals("approve"));
+        return CommonResponse.ok(HttpStatus.OK.value(), "관리자페이지 주문 취소신청서 처리 완료했습니다.");
     }
 }
