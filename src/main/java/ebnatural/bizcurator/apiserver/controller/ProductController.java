73b5ff5ef7ed507c77121c79de21a4e1c99615c3
itstimi-XD <ssk0710402@gmail.com>
2023-05-19 16:57:32 +0900

    Refactor: Mapping 내용 변경
    - GET /api/admins/products 내용을 admin controller에 추가하기 애매해서 productcontroller 내용 수정
    - GET /api/admins/products 내용 추가
    
    related to: #61

--- src/main/java/ebnatural/bizcurator/apiserver/controller/ProductController.java
+++ src/main/java/ebnatural/bizcurator/apiserver/controller/ProductController.java
@@ -1,19 +1,15 @@
 package ebnatural.bizcurator.apiserver.controller;
 
-import ebnatural.bizcurator.apiserver.domain.Product;
+import ebnatural.bizcurator.apiserver.dto.ProductAdminListDto;
 import ebnatural.bizcurator.apiserver.dto.ProductDetailDto;
-import ebnatural.bizcurator.apiserver.dto.ProductDto;
+import ebnatural.bizcurator.apiserver.dto.ProductListDto;
 import ebnatural.bizcurator.apiserver.dto.request.ProductRequest;
 import ebnatural.bizcurator.apiserver.dto.response.CommonResponse;
-import ebnatural.bizcurator.apiserver.dto.ProductListDto;
 import ebnatural.bizcurator.apiserver.service.ProductService;
-import ebnatural.bizcurator.apiserver.service.S3ImageUploadService;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,14 +23,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/products")
+@RequestMapping("/api")
 public class ProductController {
     private final ProductService productService;
 
-    @Value("${cloud.aws.s3.product-dir}")
-    private String productDir;
-
-    @PostMapping
+    @PostMapping("/products")
     public ResponseEntity<CommonResponse> createProduct(
             @RequestPart("productRequest") @Valid ProductRequest productRequest,
             @RequestPart(value = "mainImage") MultipartFile mainImage,
@@ -43,7 +36,7 @@ public class ProductController {
         productService.registerProduct(productRequest, mainImage, detailImage);
         return CommonResponse.ok(HttpStatus.CREATED.value(), "상품 생성이 완료되었습니다.");
     }
-    @GetMapping
+    @GetMapping("/products")
     public ResponseEntity<CommonResponse> getProducts(
             @RequestParam(required = false) Long categoryId,
             @RequestParam(defaultValue = "new") String sort) {
@@ -53,7 +46,16 @@ public class ProductController {
         return CommonResponse.ok(HttpStatus.OK.value(), "상품 조회가 완료되었습니다.", productMap);
     }
 
-    @GetMapping("/search")
+    @GetMapping("/admins/products")
+    public ResponseEntity<CommonResponse> getAdminProducts(
+            @RequestParam(required = false) String keyword) {
+        List<ProductAdminListDto> products = productService.getAdminProducts(keyword);
+        HashMap<String, Object> productMap = new HashMap<>();
+        productMap.put("products", products);
+        return CommonResponse.ok(HttpStatus.OK.value(), "상품 조회가 완료되었습니다.", productMap);
+    }
+
+    @GetMapping("/products/search")
     public ResponseEntity<CommonResponse> searchProducts(
             @RequestParam(required = false) String keyword,//우선 따로 예외처리 하지 않고-keyword를 입력하지않았을 때, 전체 결과를 반환한다. 필요하다면 이 부분 수정.
             @RequestParam(defaultValue = "new") String sort) {
@@ -63,7 +65,7 @@ public class ProductController {
         return CommonResponse.ok(HttpStatus.OK.value(), "상품 검색이 완료되었습니다.", productMap);
     }
 
-    @GetMapping("/{productId}")
+    @GetMapping("/products/{productId}")
     public ResponseEntity<CommonResponse> getProductDetail(@PathVariable Long productId) {
         ProductDetailDto productDetail = productService.getProductDetail(productId);
         HashMap<String, Object> productMap = new HashMap<>();
@@ -71,7 +73,7 @@ public class ProductController {
         return CommonResponse.ok(HttpStatus.OK.value(), "상품 상세 정보 조회가 완료되었습니다.", productMap);
     }
 
-    @GetMapping("/top-weekly")
+    @GetMapping("/products/top-weekly")
     public ResponseEntity<CommonResponse> getTopWeeklyProducts() {
         List<ProductListDto> products = productService.getTop3ProductsWeekly();
         HashMap<String, Object> productsMap = new HashMap<>();
@@ -79,7 +81,7 @@ public class ProductController {
         return CommonResponse.ok(HttpStatus.OK.value(), "주간 인기 상품 정보 조회가 완료되었습니다.", productsMap);
     }
 
-    @GetMapping("/top-monthly")
+    @GetMapping("/products/top-monthly")
     public ResponseEntity<CommonResponse> getTopMonthlyProducts() {
         List<ProductListDto> products = productService.getTop3ProductsMonthly();
         HashMap<String, Object> productsMap = new HashMap<>();
